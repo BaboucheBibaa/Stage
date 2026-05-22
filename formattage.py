852bf32325ebc1fc623081c28c89978d23c388d7
Lucas Delcroix <lucasdelcroix2005@gmail.com>
2026-05-22 15:02:06 +0200

    Formatage prend en compte le message IA seul.

--- formattage.py
+++ formattage.py
@@ -4,12 +4,11 @@ from json import dumps
 
 class Formattage:
     """
-    Transforme un échange (message utilisateur + réponse IA)
+    Transforme un message de l'IA
     en mémoire compacte exploitable
     """
 
-    def __init__(self, message_utilisateur: str, reponse_ia: str):
-        self.message_utilisateur = message_utilisateur
+    def __init__(self, reponse_ia: str):
         self.reponse_ia = reponse_ia
         self.message = self.__generate_output()
 
@@ -26,16 +25,13 @@ class Formattage:
 Tu es un système de mémoire conversationnelle.
 
 Ta tâche :
-Fusionner le message utilisateur et la réponse de l'IA
-pour extraire UNE information durable utile pour comprendre
-les intérêts, apprentissages ou besoins de l'utilisateur.
+Récupérer un message généré par un assistant virtuel et en extraire les informations essentielles
+
 
 RÈGLES STRICTES :
 
-- Commence obligatoirement par "L'utilisateur"
+- Répond en commencant par "l'assistant à dit"
 - Une seule phrase
-- Pas de mention de "message", "réponse", "IA" ou "conversation"
-- Ne décris jamais l’échange
 - Extrais uniquement le sens utile retenu
 - Ne jamais inventer d'information
 - Ignore politesse et contexte inutile
@@ -45,7 +41,6 @@ RÈGLES STRICTES :
                 {
                     "role": "user",
                     "content": dumps({
-                        "message_utilisateur": self.message_utilisateur,
                         "reponse_ia": self.reponse_ia
                     }, ensure_ascii=False)
                 }
@@ -58,15 +53,9 @@ RÈGLES STRICTES :
 def main():
 
     reponse_ia = """
-Bonjour Lucas, GitHub est un service permettant de gérer des projets
-collaboratifs avec repositories, branches, forks, clone et push.
-Pour commencer, il faut créer un compte, créer un repository,
-puis utiliser git clone et git push localement.
+Salut Lucas ! C'est assez cool que tu te lances dans des jeux 2D d'exploration ! En faisant preuve de patience et de persévérance, j'ai le sentiment qu'on pourrait trouver un bon choix ensemble. Par exemple, si tu aimes les jeux indépendants, tu pourrais essayer "Hollow Knight". C'est un très bon jeu qui permet de voyager dans des paysages fantastiques et de dévoiler une histoire complexe. Si tu préfères des jeux plus connus, peut-être que "Limbo" ou "Inside" te plairont ! Tu peux toujours chercher d'autres options en faisant un petit tour sur le site de jeuvideo.com pour trouver ce qui correspond le mieux à ton goût. Quoi dirais-tu de ça ?
 """
-
-    message_utilisateur = "explique moi comment utiliser github"
-
-    form = Formattage(message_utilisateur, reponse_ia)
+    form = Formattage(reponse_ia)
 
     print(form.toString())
 
